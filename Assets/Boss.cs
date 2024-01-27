d1bca8d8aa868782e26dcb2ba5683215edbb40e1
AndyCubico <99950309+AndyCubico@users.noreply.github.com>
2024-01-27 11:39:48 +0100

    Start boss clown

---
+++ Assets/Boss.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    private Animator _animator;
+    public Rigidbody2D rb;
+    public Transform playerTransform;
+
+    [Header("HP")]
+    [SerializeField] private float _HP;
+
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        playerTransform = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(float damage)
+    {
+        _HP -= damage;
+        if (_HP<=0)
+        {
+            _animator.SetTrigger("Death");
+        }
+    }
+
+    public void Death()
+    {
+        //hacer algo
+    }
+}
---
+++ Assets/Boss_move.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss_move : StateMachineBehaviour
+{
+    private Boss _boss;
+    private Rigidbody2D _rb;
+
+    [SerializeField] private float speed;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        _boss = animator.GetComponent<Boss>();
+        _rb = _boss.rb;
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        _rb.velocity = new Vector2(speed, _rb.velocity.y) * animator.transform.right;
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        _rb.velocity = new Vector2(0, _rb.velocity.y);
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
