3f3b415d001030497085a2e1050debe615eeb634
TheimerTR <albertohidalgo268@gmail.com>
2024-01-27 23:39:14 +0100

    Eyes and Blink

---
+++ Assets/Scripts/Enviroment Ambient/EyesBlink.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyesBlink : MonoBehaviour
+{
+    float timing = 0f;
+    public SpriteRenderer sprite;
+
+    private float xScale;
+
+    private void Start()
+    {
+        xScale = this.gameObject.transform.localScale.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timing += Time.deltaTime;
+
+        if (timing > Random.RandomRange(3f, 10f)) 
+        {
+            this.gameObject.transform.localScale = new Vector3(this.gameObject.transform.localScale.x - 8f * Time.deltaTime, this.gameObject.transform.localScale.y, this.gameObject.transform.localScale.z);
+            //sprite.color = new Color(sprite.color.r, sprite.color.g, sprite.color.b, sprite.color.a - Time.deltaTime);
+        }
+
+        if(this.gameObject.transform.localScale.x < 0.001f) 
+        { 
+            timing = 0f;
+            this.gameObject.transform.localScale = new Vector3(xScale, this.gameObject.transform.localScale.y, this.gameObject.transform.localScale.z);
+            //sprite.color = new Color(sprite.color.r, sprite.color.g, sprite.color.b, 1f);
+        }
+    }
+}
