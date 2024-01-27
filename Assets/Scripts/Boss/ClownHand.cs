878a16a8a7ac64fcb4bc2cb7955e7710a423d6b6
Sara-MM <sqmartinezm@gmail.com>
2024-01-27 16:39:30 +0100

    coso hand bien pero falta poner timings

---
+++ Assets/Scripts/Boss/ClownHand.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClownHand : StateMachineBehaviour
+{
+    [SerializeField] private GameObject handPrefab;
+    [SerializeField] private GameObject handInHierarchy;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        handInHierarchy = GameObject.Find(handPrefab.name).gameObject;
+        handInHierarchy.transform.Find("Curve").gameObject.SetActive(true);
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        //handPrefab.transform.GetChild(1).gameObject.SetActive(false);
+        handInHierarchy.transform.Find("Curve").gameObject.SetActive(false);
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
