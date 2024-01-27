47a060076b1e70450fdd786e655e80c6083c6717
Icefenix7198 <eric.segovia@estudiantat.upc.edu>
2024-01-27 23:51:13 +0100

    Animator ataque columas ( No funciona)

--- Assets/Scripts/Boss Animal/BossAnimalNearPilar.cs
+++ Assets/Scripts/Boss Animal/BossAnimalNearPilar.cs
@@ -4,17 +4,41 @@ using UnityEngine;
 
 public class BossAnimalNearPilar : StateMachineBehaviour
 {
+    [SerializeField] BossBeast bossScript;
+    [SerializeField] GameObject player;
+    [SerializeField] GameObject nearSpawner;
+    [SerializeField] GameObject farSpawner;
+    private float _timer = 0f;
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        player = GameObject.FindGameObjectsWithTag("Player")[0];
+        nearSpawner = GameObject.Find("PilarAttackSpawnerNear");
+        farSpawner = GameObject.Find("PilarAttackSpawnerFar");
+        _timer = 0f;
+
+    }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        _timer += Time.deltaTime;
+
+        if (_timer >= 1.0f)
+        {
+            animator.SetInteger("ChooseAttack", 0); //No se como va bien esto
+            if (player.transform.position.x > 0) 
+            {
+                SpawnColumn spawnNear = nearSpawner.GetComponent<SpawnColumn>();
+                spawnNear.spawn = true;
+            }
+            else
+            {
+                SpawnColumn spawnFar = farSpawner.GetComponent<SpawnColumn>();
+                spawnFar.spawn = true;
+            }
+        }
+    }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
