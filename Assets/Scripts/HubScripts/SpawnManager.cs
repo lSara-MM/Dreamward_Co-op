e25333e90c86095d2e72b121ff6559561079ca64
TheimerTR <albertohidalgo268@gmail.com>
2024-01-28 21:16:01 +0100

    Spot light hub

--- Assets/Scripts/HubScripts/SpawnManager.cs
+++ Assets/Scripts/HubScripts/SpawnManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class SpawnManager : MonoBehaviour
 {
@@ -8,21 +9,53 @@ public class SpawnManager : MonoBehaviour
     public GameObject player;
     public GameObject Particles;
 
+    public Light2D ligthSpot;
+
+    float timing = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
         Particles.SetActive(true);
         rb.gravityScale = 1f;
         player.transform.position = new Vector3(0f, 40f, 0f);
+        ligthSpot.intensity = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        timing += Time.deltaTime;
+
         if(rb.velocity.y == 0f) 
         {
             Particles.SetActive(false);
             rb.gravityScale = 4f;
+            ligthSpot.intensity = 3.47f;
+            timing = 0f;
+        }
+
+        if(ligthSpot.intensity > 0f) 
+        {
+            if(timing > 0.1f && timing < 0.15f) 
+            {
+                ligthSpot.intensity = 0f;
+            }
+            
+            if(timing > 0.15f && timing < 0.17f) 
+            {
+                ligthSpot.intensity = 2f;
+            }
+
+            if (timing > 0.17f && timing < 0.2f)
+            {
+                ligthSpot.intensity = 0f;
+            }
+
+            if (timing > 0.2f)
+            {
+                ligthSpot.intensity = 3.47f;
+            }
         }
     }
 }
