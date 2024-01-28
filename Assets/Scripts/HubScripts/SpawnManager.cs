6d5100fcff08553440e67e81cc0f40e2ade05e06
TheimerTR <albertohidalgo268@gmail.com>
2024-01-29 00:41:58 +0100

    Tutorial and Hub changes

--- Assets/Scripts/HubScripts/SpawnManager.cs
+++ Assets/Scripts/HubScripts/SpawnManager.cs
@@ -11,6 +11,8 @@ public class SpawnManager : MonoBehaviour
 
     public Light2D ligthSpot;
 
+    public SpriteRenderer fadeToBlack;
+
     public float timing = 0f;
 
     public bool Tickling = false;
@@ -23,12 +25,19 @@ public class SpawnManager : MonoBehaviour
         rb.gravityScale = 1f;
         player.transform.position = new Vector3(0f, 40f, 0f);
         ligthSpot.intensity = 0f;
+
+        fadeToBlack.color = new Color(0f, 0f, 0f, 1f);
     }
 
     // Update is called once per frame
     void Update()
     {
         timing += Time.deltaTime;
+
+        if (fadeToBlack.color.a > 0f) 
+        {
+            fadeToBlack.color = new Color(0f, 0f, 0f, fadeToBlack.color.a - 0.5f * Time.deltaTime);
+        }
 
         if (!Tickling)
         {
---
+++ Assets/Scripts/Tutorial/PassTutorial.cs
@@ -0,0 +1,57 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PassTutorial : MonoBehaviour
+{
+    public ChangeScene changeScene;
+    public SpriteRenderer fadeToBlack;
+
+    public GameObject canvas;
+
+    public string scene;
+    public bool secondPhase = false;
+    float timing = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.P) && Input.GetKey(KeyCode.LeftShift))
+        {
+            secondPhase = true;
+        }
+
+        if (!secondPhase) 
+        {
+            fadeToBlack.color = new Color(0f, 0f, 0f, 0f);
+            canvas.SetActive(true);
+
+            timing = 0f;
+        }
+        else 
+        {
+            timing += Time.deltaTime;
+            canvas.SetActive(false);
+
+            if (fadeToBlack.color.a < 1f) 
+            {
+                fadeToBlack.color = new Color(0f, 0f, 0f, fadeToBlack.color.a + 0.5f * Time.deltaTime);
+            }
+
+            if (timing > 2.8f && fadeToBlack.color.a >= 1f)
+            {
+                fadeToBlack.color = new UnityEngine.Color(0f, 0f, 0f, 1f);
+                timing = 0f;
+
+                changeScene.ChangeToScene(scene);
+            }
+        }
+    }
+}
