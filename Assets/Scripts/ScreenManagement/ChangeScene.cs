5394528de7e23d85f7004aae4731488dd605a777
Sara-MM <sqmartinezm@gmail.com>
2024-01-29 14:19:13 +0100

    intro scene wip

--- Assets/Scripts/ScreenManagement/ChangeScene.cs
+++ Assets/Scripts/ScreenManagement/ChangeScene.cs
@@ -5,6 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
+    [SerializeField] private bool pressAnyKey = false;
+    [SerializeField] private string passToScene;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +17,11 @@ public class ChangeScene : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (pressAnyKey && Input.anyKeyDown && !(Input.GetMouseButtonDown(0) 
+            || Input.GetMouseButtonDown(1) || Input.GetMouseButtonDown(2)))
+        {
+            ChangeToScene(passToScene);
+        }
     }
 
     public void ChangeToScene(string passToScene)
