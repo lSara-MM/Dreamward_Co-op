8a568bc8254c066118c90e38b1fbd875d925b0fa
Icefenix7198 <eric.segovia@estudiantat.upc.edu>
2024-01-30 23:28:51 +0100

    Boss 2 fix HP no gener
    Fix final lobo cambio dase 2

--- Assets/Scripts/BossHealth.cs
+++ Assets/Scripts/BossHealth.cs
@@ -43,11 +43,6 @@ public class BossHealth : MonoBehaviour
             Debug.Log("SA ENFADAO");
         }
 
-        if (!_animator.GetBool("Enraged") && bossSP)
-        {
-            _animator.SetBool("Enraged", bossSP);
-        }
-
         slider.value = currentHealth;
 
         // Debug
