d76e45ab85964b57a73890fb2caadb9965512801
AndyCubico <99950309+AndyCubico@users.noreply.github.com>
2024-01-30 16:23:41 +0100

    Gamepad control text

--- Assets/Scripts/DialogueSelector.cs
+++ Assets/Scripts/DialogueSelector.cs
@@ -56,7 +56,7 @@ public class DialogueSelector : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)) // Andreu pon aquí el control para el mando para Skipear
+        if (Input.GetButtonDown("Jump"))// button jump = space/A button
         {
             if (Dialogue.Value == 0)
             {
