57e697e883168424a0a9d990d0b9f9185888b212
AndyCubico <99950309+AndyCubico@users.noreply.github.com>
2024-01-30 20:57:33 +0100

    Clown muelto

---
+++ Assets/Scripts/Boss Clown/BossKillHand.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossKillHand : StateMachineBehaviour
+{
+    private Animator _bossAnimator;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        _bossAnimator = GameObject.Find("Enemy").transform.gameObject.GetComponent<Animator>();
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (_bossAnimator.GetCurrentAnimatorStateInfo(0).IsName("Death"))
+        {
+            animator.SetTrigger("Death");
+        }
+
+        if (_bossAnimator.GetCurrentAnimatorStateInfo(0).IsName("BossWin"))
+        {
+            animator.SetTrigger("BossWins");
+        }
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
