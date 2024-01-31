f903a839d9b8e53e6e9478edd546acca41d4f211
TheimerTR <albertohidalgo268@gmail.com>
2024-01-31 14:27:21 +0100

    SecondPhase

---
+++ Assets/Scripts/Boss Cloud 1/LightingAmbient.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightingAmbient : MonoBehaviour
+{
+    public GameObject[] lightings;
+    int actualLigth = 0;
+    float timing = 0f;
+    float coolDown = 0f;
+
+    bool waitLight = false;
+
+    float minTime = 0f; // Tiempo minimo que tarda un rayo en aparecer
+    float maxTime = 5f; // Tiempo máximo que tarda un rayo en aparecer
+    float appearenceTime = 0.3f; //Tiempo que un rayo se mantiene encendido
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        actualLigth = Random.RandomRange(0, lightings.Length);
+
+        for(int i = 0; i < lightings.Length; i++)
+        {
+            lightings[i].SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timing += Time.deltaTime;
+
+        if (!waitLight)
+        {
+            if (timing >= Random.RandomRange(minTime, maxTime))
+            {
+                actualLigth = Random.RandomRange(0, lightings.Length);
+                waitLight = true;
+            }
+        }
+
+        if (waitLight) 
+        { 
+            coolDown += Time.deltaTime;
+
+            lightings[actualLigth].SetActive(true);
+
+            if (coolDown >= appearenceTime)
+            {
+                lightings[actualLigth].SetActive(false);
+                coolDown = 0f;
+                timing = 0f;
+                waitLight = false;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Boss Cloud 1/SecondPhaseBoss3.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class SecondPhaseBoss3 : MonoBehaviour
+{
+    public Light2D globalLight;
+    public Light2D bossEye1;
+    public Light2D bossEye2;
+    public Light2D bossEye3;
+
+    public Light2D Lamp1;
+    public Light2D Lamp2;
+
+    public GameObject Lightings;
+
+    [SerializeField] private BossHealth bossHealth;
+
+    float timing = 0f;
+
+    bool isInSecondFase = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.P) && Input.GetKey(KeyCode.LeftShift))
+        {
+            bossHealth.bossSP = true;
+        }
+
+        if (!bossHealth.bossSP) 
+        {
+            globalLight.intensity = 2.41f;
+            globalLight.color = new Color(0.580f, 0.654f, 0.764f, 1f);
+
+            Lamp1.intensity = 0.66f;
+            Lamp1.color = new Color(0.717f, 0.559f, 0.152f, 1f);
+            
+            Lamp2.intensity = 0.66f;
+            Lamp2.color = new Color(0.717f, 0.559f, 0.152f, 1f);
+
+            bossEye1.intensity = 0.57f;
+            bossEye2.intensity = 0.57f;
+            bossEye3.intensity = 0.57f;
+
+            Lightings.SetActive(false);
+
+            timing = 0f;
+        }
+        else 
+        {
+            if (!isInSecondFase)
+            {
+                timing += Time.deltaTime;
+
+                Lightings.SetActive(true);
+
+                if (globalLight.intensity > 0f)
+                {
+                    globalLight.intensity -= 0.005f * timing;
+                }
+
+                if (timing > 2.8f)
+                {
+                    globalLight.intensity = 1.8f;
+                    globalLight.color = new Color(0.580f, 0.654f, 0.764f, 1);
+                    timing = 0f;
+
+                    ChangeLigth();
+
+                    isInSecondFase = true;
+                }
+            }
+        }
+    }
+
+    void ChangeLigth() 
+    {
+        Lamp1.intensity = 3.23f;
+        Lamp1.color = new Color(0.213f, 0.034f, 0.273f, 1f);
+
+        Lamp2.intensity = 3.23f;
+        Lamp2.color = new Color(0.213f, 0.034f, 0.273f, 1f);
+
+        bossEye1.intensity = 1.86f;
+        bossEye2.intensity = 1.86f;
+        bossEye3.intensity = 0.63f;
+    }
+}
