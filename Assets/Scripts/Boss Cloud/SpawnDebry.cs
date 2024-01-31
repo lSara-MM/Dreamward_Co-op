c8b4e2655e0d287ca86e891ef965cbbd480f1874
Icefenix7198 <eric.segovia@estudiantat.upc.edu>
2024-01-31 22:26:40 +0100

    Wind attack balance

--- Assets/Scripts/Boss Cloud/SpawnDebry.cs
+++ Assets/Scripts/Boss Cloud/SpawnDebry.cs
@@ -25,13 +25,16 @@ public class SpawnDebry : MonoBehaviour
     [Header("Spawner Attributes")]
     [SerializeField] private float realFiringRate = 1f;
 
+
     private GameObject spawnedBullet;
     private float timer = 0f;
+    private bool activeAttack = false;
     // Start is called before the first frame update
     void Start()
     {
         //realFiringRate = 1.0f;
         _animator = _boss.GetComponent<Animator>();
+        activeAttack = false;
     }
 
     int random;
@@ -40,22 +43,33 @@ public class SpawnDebry : MonoBehaviour
     void Update()
     {
         timer += Time.deltaTime;
-        if (bossState.bossSP && _animator.GetInteger("ChooseAttack") == 6 + ((int)direction)) 
+
+        //Activate if it was inactive and is its time to attack
+        if (!activeAttack && _animator.GetInteger("ChooseAttack") == 6 + ((int)direction)) 
+        {
+            activeAttack = true;
+            random = Random.Range(-1, 2); //If not boss phase 2 only chooses one type of wind scycthes at a time
+        }
+
+        //If atacando make wind push
+        if (_animator.GetInteger("ChooseAttack") == 6 + ((int)direction)) 
         {
             if (!wind.isPlaying) { wind.Play(); }
-            WindPush();
+            WindPush(bossState.bossSP);
         }
-        else 
+        else //if not attacking stop wind
         {
             wind.Stop();
         }
+
+        //Shot wind sycthes
         if (timer >= realFiringRate)
         {
             soundSource.Stop();
             Fire();
             timer = 0.0f;
         }
-        if(timer == 0.0f) 
+        if(timer == 0.0f && bossState.bossSP && activeAttack) 
         {
             random = Random.Range(-1, 2);
             if (bullet && _animator.GetInteger("ChooseAttack") == 6 + ((int)direction)) 
@@ -63,6 +77,12 @@ public class SpawnDebry : MonoBehaviour
                 soundSource.clip = sounds[random + 1];
                 soundSource.Play();
             }
+        }
+
+        //If not longer attacking deactivate
+        if (_animator.GetInteger("ChooseAttack") != 6 + ((int)direction)) 
+        {
+            activeAttack = false;
         }
     }
 
@@ -97,6 +117,12 @@ public class SpawnDebry : MonoBehaviour
                         scale = 1.3f;
                         break;
                     }
+                default: 
+                    {
+                        speedMod = 1.0f;
+                        scale = 1.0f;
+                        break;
+                    }
             }
 
             spawnedBullet = Instantiate(bullet, spawnPos, Quaternion.identity);
@@ -111,7 +137,7 @@ public class SpawnDebry : MonoBehaviour
     }
 
     float delayWind = 0.0f;
-    private void WindPush()
+    private void WindPush(bool secondPhase)
     {
         delayWind += Time.deltaTime;
         if (delayWind>= 0.2f) 
@@ -120,11 +146,14 @@ public class SpawnDebry : MonoBehaviour
             newPosplayer = _player.transform.position;
             if (_player.transform.localScale.x / Mathf.Abs(_player.transform.localScale.x) == direction) 
             {
-                newPosplayer.x += 0.25f * direction;
+                if (secondPhase) { newPosplayer.x += 0.35f * direction; }
+                else { newPosplayer.x += 0.15f * direction; }
             }
             else 
             {
-            newPosplayer.x += 0.08f*direction;
+                if (secondPhase) {newPosplayer.x += 0.15f*direction; }
+                else { newPosplayer.x += 0.08f * direction; }
+                    
             }
             
             _player.transform.position = newPosplayer;
