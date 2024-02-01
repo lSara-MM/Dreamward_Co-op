777790ea2699e3904192321bdc3d2770fe41c5b1
Icefenix7198 <eric.segovia@estudiantat.upc.edu>
2024-02-01 07:39:32 +0100

    TEXTURA CUCHILLA

--- Assets/Scripts/Boss Cloud/Animator/BossCloudIdle.cs
+++ Assets/Scripts/Boss Cloud/Animator/BossCloudIdle.cs
@@ -8,8 +8,7 @@ public class BossIdle : StateMachineBehaviour
     public float realWait = 0.0f;
     [SerializeField] float idleTime = 0.0f;
     [SerializeField] int moves = 3;
-    [SerializeField] private int firstCommand = 9;
-    [SerializeField] private int secondCommand = 9;
+    [SerializeField] private int lastCommand = 9;
     private BossHealth _Boss;
     public Vector3[] positions; //Positions at where the cloud boss will move
     int posToMove = 0;
@@ -38,30 +37,17 @@ public class BossIdle : StateMachineBehaviour
         if (idleTime >= realWait)
         {
             //Lateral attack D (0), lateral attack I (1), Strong Rain (2)
+
+            int randNum = Random.Range(0, moves);
+
+            while (animator.GetInteger("ChooseAttack") == lastCommand) 
+            {
+                animator.SetInteger("ChooseAttack", randNum);
+                if
+            }
             
-            animator.SetInteger("ChooseAttack", Random.Range(0, moves));
-            while(secondCommand == animator.GetInteger("ChooseAttack") || secondCommand==-1 && (animator.GetInteger("ChooseAttack") > 2)) 
-            {
-                animator.SetInteger("ChooseAttack", Random.Range(0, moves));
-            }
-            if(firstCommand == animator.GetInteger("ChooseAttack")) // Guardar si a repetido el ataque anterior
-            {
-                secondCommand = firstCommand;
-            }
-            else if(firstCommand < 0 && (animator.GetInteger("ChooseAttack") == 0 || animator.GetInteger("ChooseAttack") == 1)) //Guardar como -1 si uso un ataque de viento antes y ahora
-            {
-                secondCommand = -1;
-            }
-            else //Si no ha sido el segundo comando igual que el primero reseteamos todo
-            {
-                secondCommand = 9;
-                firstCommand = 9;
-            }
-            if (firstCommand == 9) //Estado base donde cojemos el ataque que acaba de hacer
-            { 
-                firstCommand = animator.GetInteger("ChooseAttack");
-                if(firstCommand<2) { firstCommand = -1; } //Debido a que wind tiene dos numeros lo guardamos como un -1
-            }            
+            lastCommand = animator.GetInteger("ChooseAttack");
+
 
             idleTime = 0.0f;
         }
