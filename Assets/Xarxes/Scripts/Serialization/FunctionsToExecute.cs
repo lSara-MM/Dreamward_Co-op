36486af7fb9dc0bfd482502ed4357c35e67a1b94
Sara-MM <sqmartinezm@gmail.com>
2024-11-17 00:30:17 +0100

    Update FunctionsToExecute.cs

--- Assets/Xarxes/Scripts/Serialization/FunctionsToExecute.cs
+++ Assets/Xarxes/Scripts/Serialization/FunctionsToExecute.cs
@@ -105,7 +105,7 @@ public class FunctionsToExecute : MonoBehaviour
     public void ExecuteInput(SerializedData<ns_structure.playerInput> data)
     {
         ns_structure.playerInput param = data.parameters;
-        Debug.Log($"Execute Input: {param.key}");
+        //Debug.Log($"Execute Input: {param.key}");
 
         GameObject go = guidDictionary[data.network_id];
 
