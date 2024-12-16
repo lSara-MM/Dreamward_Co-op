cbfa98414caecf97cd4cea0dc68c5e5e0e46f4b5
lSara-MM <sqmartinezm@gmail.com>
2024-12-16 11:43:08 +0100

    spawn enemy
    Co-Authored-By: Andreu Nosàs Soler <99950309+AndyCubico@users.noreply.github.com>

---
+++ Assets/Xarxes/Scripts/Others/SpawnObject.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObject : MonoBehaviour
+{
+    [SerializeField] private GUID_Generator cs_guid;
+
+    [SerializeField] private string serverPrefabPath;
+    [SerializeField] private Vector3 spawnServerPosition;
+
+    [SerializeField] private string clientPrefabPath;
+    [SerializeField] private Vector3 clientServerPosition;
+
+    GameObject online;
+    Serialization cs_Serialization;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Check if server or client
+        if (online = GameObject.FindGameObjectWithTag("Server"))
+        {
+            GameObject prefab = Resources.Load(serverPrefabPath) as GameObject;
+
+            GameObject go = Instantiate(prefab, spawnServerPosition, Quaternion.identity);
+            cs_guid = go.GetComponent<GUID_Generator>();
+
+            cs_Serialization.SerializeData((cs_guid != null ? cs_guid.GetGuid() : default), ACTION_TYPE.SPAWN_OBJECT,
+                new ns_structure.spawnPrefab(clientPrefabPath, clientServerPosition));
+
+        }
+        else if (online = GameObject.FindGameObjectWithTag("Client"))
+        {
+            //cs_Serialization.SerializeData((cs_guid != null ? cs_guid.GetGuid() : default), ACTION_TYPE.SPAWN_OBJECT,
+            //    new ns_structure.spawnPrefab(clientPrefabPath, clientServerPosition));
+        }
+        else
+        {
+            Debug.Log("Online not found");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
